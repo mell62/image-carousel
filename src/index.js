a5a8e1fc3d82c7d1307d51f6de5a197218c1f010
Mridhul <mridhul67@gmail.com>
2024-09-14 02:54:13 +0530

    Implement functionality to browse to the next or previous image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./style.css";
 import "./modern-normalize.css";
+
+import { getNextPicture, getPreviousPicture } from "./main";
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("left-arrow")) {
+    getPreviousPicture();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("right-arrow")) {
+    getNextPicture();
+  }
+});
